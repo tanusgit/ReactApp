4224fa6b0b0903fbd3191c9f45cae1a2d258a392
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-20 12:16:46 -0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Menu from './component/Menu';
+import Hello from './component/Hello';
 
 function App() {
   return (
     <div className="App">
+      hello
     <Menu></Menu>
+    <Hello></Hello>
     </div>
   );
 };
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Hello extends Component{
 
+    constructor(props){
+      super(props);
+    }
+render(){
+    return (
+    <p>hello world</p>
+    );
+}
 
-
+}
+export default Hello;
