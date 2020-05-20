ec9e2dab9522eda93e4ad2a1640daaa23c1e7e9e
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-20 11:34:57 -0700

    created menu bar

---
+++ src/Readme.md
@@ -0,0 +1,6 @@
+bootstrap for react :
+https://react-bootstrap.github.io/components/navbar/
+
+https://reactstrap.github.io/components/media/
+
+https://reactstrap.github.io/components/navbar/
---
+++ src/component/Menu.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+
+class Menu extends Component{
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+          </Navbar>
+    </div>
+    );
+  }
+
+}
+
+export default Menu;
